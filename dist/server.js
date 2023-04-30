71c4420ac83fd153680d8f16db1fd406fd4569c8
MatheDevGr <matheus.reis@modalgr.com.br>
2023-04-30 12:12:22 -0300

    fix: ajuste server.js

--- dist/server.js
+++ dist/server.js
@@ -1,19 +1,19 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.set('strictQuery', false);
-const app = (0, express_1.default)();
-mongoose_1.default.connect("mongodb://localhost/ecommerce");
-app.use(express_1.default.json());
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/ecommerce");
+
+app.use(express.json());
 app.use(routes);
+
 const PORT = 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
-function routes(routes) {
-    throw new Error("Function not implemented.");
+
+function routes() {
+  throw new Error("Function not implemented.");
 }
