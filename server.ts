7db5d9892c4764475624031afdf97f041c2f9b11
MatheDevGr <matheus.reis@modalgr.com.br>
2023-04-30 10:04:36 -0300

    fix: ajuste json

--- server.ts
+++ server.ts
@@ -2,7 +2,6 @@ import express from "express";
 import mongoose from "mongoose";
 import routes from "./src/routes";
 
-
 const app = express();
 
 mongoose.connect("mongodb://localhost/ecommerce");
@@ -10,6 +9,8 @@ mongoose.connect("mongodb://localhost/ecommerce");
 app.use(express.json());
 app.use(routes);
 
-app.listen(3000, () => {
-  console.log("Server is listening on port 3000");
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
